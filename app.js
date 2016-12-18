98e45b9fcf72a137c20a3ed5d8fffc7ebd4677b8
“kasper341” <kasper_341@hotmail.com>
2016-12-18 15:29:52 -0800

    add ajax to add "addServer()" functions

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    //cluster domain collection
+    var mesosDomain = [];
+
+    function addServer(event) {
+
+        console.log(event);
+
+        $.ajax("server_instance/_server_instance_tepm.html" ,
+		      {dataType: "text"}
+        )
+		.then(function(contents){
+			$content.append(contents);
+		});
+
+        mesosDomain.push();
+    }
+
+    function destroyServer(event) {
+        event.preventDefault();
+        console.log("destroy clicked");
+
+    }
+
+    var $content = $("#server-canvas-content");
+
+    $("#add-server").on("click", addServer);
+
+    $("#destroy").on("click", destroyServer);
+
 });
