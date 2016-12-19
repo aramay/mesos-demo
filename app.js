11daf54f8fb94144af5483ec23bee01c2c70e809
Abid Ramay <kasper_341@hotmail.com>
2016-12-19 13:11:15 -0800

    Update app.js
    update path to file

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ $( document ).ready(function() {
     function addServer(event) {
 
         $.ajax({
-            url: "/server_instance/_server_instance_temp.html" ,
+            url: "mesos-demo/server_instance/_server_instance_temp.html" ,
 		     dataType: "text",
 		async: false
          })
